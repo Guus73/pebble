7b312ddb44d20aeea9f1d8ecb6590de147324fc6
Guus73 <guus@eldering.net>
2014-02-08 22:19:28 +0100

    Update Pebble.js

--- Pebble.js
+++ Pebble.js
@@ -1,3 +1,2 @@
-
-<script src="http://www.ecstorage.nl:8888/shares/Solar%20data/sunprofit.js"></script>
+<script src="http://www.ecstorage.nl:8888/shares/Solar%20data/sunprofit.js">
 simply.title(totalyieldkwh_t());
